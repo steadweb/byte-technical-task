5643519ba88e5daf8066aee2af2fe0928d986e98
Luke Steadman <luke@steadweb.co.uk>
2020-06-08 11:59:01 +0100

    Use stead-bot rather than 6 for bot_id

--- solutions/04-sql.md
+++ solutions/04-sql.md
@@ -5,7 +5,7 @@ I'd move the `StateName` from the `event_data` jsonb column, and create it's own
 ```sql
 SELECT COUNT(*) as count FROM app_events
 WHERE state_name IN ('reorder', 'reorder-allergy-info', 'reorder-pay')
-AND bot_id = '6' AND EXTRACT(MONTH FROM created_at) = 6;
+AND bot_id = 'stead-bot' AND EXTRACT(MONTH FROM created_at) = 6;
 ```
 
 _Note: the prepared statement also needs updating, the command for this is below._
@@ -55,11 +55,11 @@ Create some dummy data to test the above SELECT query out
 
 ```sql
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data, state_name)
-VALUES ('6', 1, '123', 'foo', 'foo', '{}', 'reorder-allergy-info');
+VALUES ('stead-bot', 1, '123', 'foo', 'foo', '{}', 'reorder-allergy-info');
 
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data, state_name)
-VALUES ('6', 2, '123', 'foo', 'foo', '{}', 'reorder');
+VALUES ('stead-bot', 2, '123', 'foo', 'foo', '{}', 'reorder');
 
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data, state_name)
-VALUES ('6', 3, '123', 'foo', 'foo', '{}', 'reorder-pay');
+VALUES ('stead-bot', 3, '123', 'foo', 'foo', '{}', 'reorder-pay');
 ```
--- solutions/04-sql.sql
+++ solutions/04-sql.sql
@@ -1,7 +1,7 @@
 /* Example SQL command */
 SELECT COUNT(*) as count FROM app_events 
 WHERE event_data ->> 'StateName' IN ('reorder', 'reorder-allergy-info', 'reorder-pay') 
-AND bot_id = '6' AND EXTRACT(MONTH FROM created_at) = 6;
+AND bot_id = 'stead-bot' AND EXTRACT(MONTH FROM created_at) = 6;
 
 /* Prepated statement using the above SQL */
 PREPARE find_events (text, int) AS
@@ -10,17 +10,17 @@ PREPARE find_events (text, int) AS
     AND bot_id = $1 AND EXTRACT(MONTH FROM created_at) = $2;
 
 /* Example usage of find_events prepared statement */
-EXECUTE find_events('6', 6);
+EXECUTE find_events('stead-bot', 6);
 
 /* INSERTS to created dummy data */ 
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data)
-VALUES ('6', 1, '123', 'foo', 'foo', '{ "StateName": "reorder-allergy-info" }');
+VALUES ('stead-bot', 1, '123', 'foo', 'foo', '{ "StateName": "reorder-allergy-info" }');
 
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data)
-VALUES ('6', 2, '123', 'foo', 'foo', '{ "StateName": "reorder" }');
+VALUES ('stead-bot', 2, '123', 'foo', 'foo', '{ "StateName": "reorder" }');
 
 INSERT INTO app_events (bot_id, event_id, aggregate_type, aggregate_id, event_type, event_data)
-VALUES ('6', 3, '123', 'foo', 'foo', '{ "StateName": "reorder-pay" }');
+VALUES ('stead-bot', 3, '123', 'foo', 'foo', '{ "StateName": "reorder-pay" }');
 
 /* Note: I fixed the PRIMARY KEY bug as primary key app_events_pkey (event_id) isn't valid : */
 create table app_events (
